a1808a66ff93ba052d14dfb840b0aa8d96a45769
Bondaliname <lina@Lenovo-Thinkpad.>
2025-04-10 12:09:44 +0000

    Solution

--- task.sh
+++ task.sh
@@ -1,1 +1,11 @@
 #! /bin/bash
+
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB > ShopDB_full_backup.sql
+
+mysql -u "$DB_USER" -p"$DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS ShopDBReserve;"
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < ShopDB_full_backup.sql
+
+mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-info ShopDB > ShopDB_data_backup.sql
+
+mysql -u "$DB_USER" -p"$DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS ShopDBDevelopment;"
+mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < ShopDB_data_backup.sql
