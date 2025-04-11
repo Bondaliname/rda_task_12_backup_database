ecb8df3a88a7604fceb466af1e1ddff9708e72b1
Bondaliname <lina@Lenovo-Thinkpad.>
2025-04-11 07:39:37 +0000

    Solution

--- task.sh
+++ task.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 
+echo "Creating full backup of ShopDB..."
 mysqldump -u "$DB_USER" -p"$DB_PASSWORD" ShopDB > ShopDB_full_backup.sql
 
+echo "Restoring full backup of ShopDB..."
 mysql -u "$DB_USER" -p"$DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS ShopDBReserve;"
 mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBReserve < ShopDB_full_backup.sql
 
+echo "Creating data-only backup of ShopDB..."
 mysqldump -u "$DB_USER" -p"$DB_PASSWORD" --no-create-info ShopDB > ShopDB_data_backup.sql
 
+echo "Restoring data-only backup to ShopDBDevelopment..."
 mysql -u "$DB_USER" -p"$DB_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS ShopDBDevelopment;"
 mysql -u "$DB_USER" -p"$DB_PASSWORD" ShopDBDevelopment < ShopDB_data_backup.sql
+
+echo "Backup and restoration completed successfully."
